8de2cdb3738d8c1a6b0b94b8fd89c2e7da8e6022
Amy Shackles <amyshackles@gmail.com>
2019-02-26 23:48:50 -0800

    Create ArithmeticOperators.js

---
+++ HackerRank/10DaysOfJavascript/Day1/ArithmeticOperators.js
@@ -0,0 +1,17 @@
+/* 
+- Write a function, getArea(length, width) which returns the area.
+- Write a function, getPerimeter(length, width) which returns the perimeter.
+
+*/
+
+function getArea(length, width) {
+  let area;
+  area = length * width;
+  return area;
+}
+
+function getPerimeter(length, width) {
+  let perimeter;
+  perimeter = 2 * (length + width);
+  return perimeter;
+}
